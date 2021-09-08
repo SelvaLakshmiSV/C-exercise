0b4d37168a30e7b3f1a7b17eacb40c4bc1f5a77b
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-09-08 16:52:19 +0530

    Understand datatypes

---
+++ sum of even and odd.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int number1,number2,number3,number4,number5,number6,number7,number8;
+    cin>>number1>>number2>>number3>>number4>>number5>>number6>>number7>>number8;
+    cout<<number2+number4+number6+number8<<" "<<number1+number3+number5+number7;
+    return 0;
+}

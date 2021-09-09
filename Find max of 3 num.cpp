ae6cbd2282f71a4f4764412463fc86578e4b5c06
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-09-09 11:02:41 +0530

    Max of 3 number if <100

---
+++ Find max of 3 num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+    int a,b,c;
+    cin>>a>>b>>c;
+    int res = -1;
+    if(a<100&&a>res)
+       res = a;
+    if(b<100&&b>res)
+       res = b;
+    if(c<100&&c>res)
+       res = c;
+    cout<<" "<<res<<" ";
+}

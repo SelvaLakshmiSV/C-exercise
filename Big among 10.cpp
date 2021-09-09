c0291c44cab2483aefc11cf397ecd067cee10e78
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-09-09 11:38:40 +0530

    Find out biggest among 10 numbers

---
+++ Big among 10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   int num,res;
+    cin>>res;
+    cin>>num;if(num>res) res = num;
+    cin>>num;if(num>res) res = num;
+    cin>>num;if(num>res) res = num; 
+    cin>>num;if(num>res) res = num;
+    cin>>num;if(num>res) res = num;
+    cin>>num;if(num>res) res = num;
+    cin>>num;if(num>res) res = num;
+    cin>>num;if(num>res) res = num;
+    cin>>num;if(num>res) res = num;
+    cout<<res;
+    
+}
